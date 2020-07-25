b9b78787477371abe5f5e967c0a7a3ee9e5b8b31
junjun <zhangjunjun1989@163.com>
2020-07-25 20:42:45 +0800

    fix: 修改代码

--- src/test/java/com/iflyteck/test/AccountTest.java
+++ src/test/java/com/iflyteck/test/AccountTest.java
@@ -23,12 +23,6 @@ public class AccountTest {
     @Autowired
     private AccountService accountService;
 
-//    @Before
-//    public void init () {
-//        ca = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-//        accountService = ca.getBean("accountServiceImpl", AccountServiceImpl.class);
-//    }
-
     @Test
     public void testFindAll () {
         List<Account> accounts = accountService.findAllAccount();
